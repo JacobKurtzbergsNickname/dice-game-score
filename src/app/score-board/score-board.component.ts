a908788c7f7930dec0ec79bf50b864693ddb85e3
Korbinian Brunner <85752115+JacobKurtzbergsNickname@users.noreply.github.com>
2026-02-16 19:58:51 +0100

    Refactor score app components and strengthen tests

--- src/app/score-board/score-board.component.ts
+++ src/app/score-board/score-board.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { ScoreTileComponent } from "../score-tile/score-tile.component";
+
+import { ScoreTileComponent } from '../score-tile/score-tile.component';
 
 @Component({
   selector: 'pairodice-score-board',
   standalone: true,
   imports: [ScoreTileComponent],
   templateUrl: './score-board.component.html',
-  styleUrl: './score-board.component.css'
+  styleUrl: './score-board.component.css',
 })
 export class ScoreBoardComponent {
-
-  numberOfTiles = 1;
-
+  readonly numberOfTiles = 4;
+  readonly tiles = Array.from({ length: this.numberOfTiles }, (_, index) => index);
 }
