f61e1163089d441e5de646721ea665d1b3af2e2d
Kirby <korbinian.brunner@web.de>
2026-03-20 11:53:28 +0100

    Lints and creates pre-commit hook

--- src/app/actual-score/actual-score.component.spec.ts
+++ src/app/actual-score/actual-score.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActualScoreComponent } from './actual-score.component';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('ActualScoreComponent', () => {
   let component: ActualScoreComponent;
